636df1942fc4d02698a7c0d4f629d532825ab653
René Link <rene.link@link-intersystems.com>
2022-08-12 09:16:33 +0200

    Don't need release scripts anymore with lis-central-jar:8.4.

--- release-perform.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn release:perform -P release -DlocalCheckout=true -Darguments="-Dmaven.site.skip=true -Dmaven.site.deploy.skip=true"
--- release-prepare.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn -P release,compatibility release:prepare -DpushChanges=false
